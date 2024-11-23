cc7e4ce30707643f346e8de8237ba594b95aeae4
Gerrit M. Keller <gerrit.keller@gmail.com>
2024-11-23 19:07:18 +0100

    [2015] Go: Day 3 part2

--- 2015/go/day03.go
+++ 2015/go/day03.go
@@ -13,7 +13,61 @@ const south = 'v'
 const east = '>'
 const west = '<'
 
-func main() {
+func part2() {
+	var santaPosA int
+	var santaPosB int
+	var robosantaPosA int
+	var robosantaPosB int
+
+	var m map[string]int
+	m = make(map[string]int)
+	m[fmt.Sprintf("%d_%d", 0, 0)] = 1
+
+	f, err := os.Open(inputFile)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	var count int
+	reader := bufio.NewReader(f)
+	for {
+		c, _, err := reader.ReadRune()
+		if err != nil {
+			break
+		}
+		switch count%2 {
+		case 0:
+			switch c {
+			case north:
+				santaPosA++
+			case south:
+				santaPosA--
+			case east:
+				santaPosB++
+			case west:
+				santaPosB--
+			}
+			m[fmt.Sprintf("%d_%d", santaPosA, santaPosB)] = 1
+		case 1:
+			switch c {
+			case north:
+				robosantaPosA++
+			case south:
+				robosantaPosA--
+			case east:
+				robosantaPosB++
+			case west:
+				robosantaPosB--
+			}
+			m[fmt.Sprintf("%d_%d", robosantaPosA, robosantaPosB)] = 1
+		}
+		count++
+	}
+	fmt.Println("Houses visited (year++):", len(m))
+}
+
+func part1() {
 	var posA int
 	var posB int
 
@@ -46,4 +100,9 @@ func main() {
 		m[fmt.Sprintf("%d_%d", posA, posB)] = 1
 	}
 	fmt.Println("Houses visited:", len(m))
+}
+
+func main() {
+	part1()
+	part2()
 }
